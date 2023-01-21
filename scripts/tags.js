0e05ee6ea82250b3c22c23b532a7e72935b8bcbb
Maxime <maxime.sciare@gmail.com>
2023-01-21 17:33:22 +0100

    added infos on tags + testimonies scrollbar

--- scripts/tags.js
+++ scripts/tags.js
@@ -2,6 +2,7 @@
 
 const aboutZone = document.querySelector(".about-me");
 const allTags = Array.from(document.querySelectorAll(".tag"));
+const allTagsInfos = Array.from(document.querySelectorAll(".tag-desc"));
 
 aboutZone.addEventListener("mousemove", (e) => {
 
@@ -15,8 +16,25 @@ aboutZone.addEventListener("mousemove", (e) => {
     })
 })
 
-allTags.forEach(tag => {
+allTags.forEach((tag, tagIndex) => {
+    
     tag.addEventListener("click", () => {
-        tag.classList.toggle("active");
+
+        // Was the tag active before click ?
+        const isActive = tag.classList.contains("active");
+
+        // Remove all previous actives tags
+        allTags.forEach((tag, tagIndex) => {
+            tag.classList.remove("active");
+            allTagsInfos[tagIndex].classList.remove('active');
+        })
+
+
+        // If the current tag wasn't active before, we make it active
+        if (!isActive) {
+            tag.classList.add("active");
+            allTagsInfos[tagIndex].classList.add('active');
+        }
+        
     })
 })
